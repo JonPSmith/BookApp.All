6ea1766662d017e4c6c606b2cd81b6aaf81b11c5
Jon P Smith <jon.smith@selectiveanalytics.com>
2021-05-02 10:26:06 +0100

    Fixed bug

--- Test/UnitTests/TestModularMonolithRules.cs
+++ Test/UnitTests/TestModularMonolithRules.cs
@@ -59,10 +59,10 @@ namespace Test.UnitTests
 
             //ATTEMPT
             var bContextDir = _rulesChecker.GetBoundedContextLayerWithTheirReferencedProjectNames();
-            foreach (var firstBContextNames in bContextDir.Keys.Where(x => x != NameToAllowMultipleAccessed))
+            foreach (var firstBContextNames in bContextDir.Keys)
             {
                 foreach (var secondBContextNames in bContextDir.Keys
-                    .Where(x => x != NameToAllowMultipleAccessed && x != firstBContextNames))
+                    .Where(x => x != firstBContextNames))
                 {
                     var badOverlaps = bContextDir[firstBContextNames]
                         .Where(x => bContextDir[secondBContextNames].Contains(x)
